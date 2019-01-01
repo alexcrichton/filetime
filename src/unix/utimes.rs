4718343fac3064ec6c265b8ccef5ce92af740dc2
Anton Lazarev <antonok35@gmail.com>
2018-12-31 22:04:03 -0500

    fix cfg attributes for macos and others

--- src/unix/utimes.rs
+++ src/unix/utimes.rs
@@ -5,9 +5,13 @@ use FileTime;
 use super::libc;
 
 pub fn set_file_times(p: &Path, atime: Option<FileTime>, mtime: Option<FileTime>) -> io::Result<()> {
-    super::utimes(p, atime, mtime, libc::utimes)
+    // Safe to unwrap atime and mtime -- platforms which use the utimes module
+    // only expose public API functions to set both at once
+    super::utimes(p, atime.unwrap(), mtime.unwrap(), libc::utimes)
 }
 
 pub fn set_symlink_file_times(p: &Path, atime: Option<FileTime>, mtime: Option<FileTime>) -> io::Result<()> {
-    super::utimes(p, atime, mtime, libc::lutimes)
+    // Safe to unwrap atime and mtime -- platforms which use the utimes module
+    // only expose public API functions to set both at once
+    super::utimes(p, atime.unwrap(), mtime.unwrap(), libc::lutimes)
 }
